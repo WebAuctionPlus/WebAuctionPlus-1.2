81710efe199de9dc22988535c17db712743354d8
lorenzop <lorenzo@poixson.com>
2015-03-17 08:48:40 -0400

    cleaned up Language class added this. where missing removed langConfig object from local class storage reformatted loadKeys() function

--- java/src/me/lorenzop/webauctionplus/Language.java
+++ java/src/me/lorenzop/webauctionplus/Language.java
@@ -15,23 +15,21 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Language {
 
-	protected HashMap<String, String> langMap = new HashMap<String, String>();
-	protected FileConfiguration langConfig = null;
+	protected final HashMap<String, String> langMap = new HashMap<String, String>();
 
 	protected final List<String> words = new ArrayList<String>();
 
 	protected final WebAuctionPlus plugin;
-	private boolean isOk;
+	private volatile boolean isOk = false;
 
 	public Language(final WebAuctionPlus plugin) {
 		this.plugin = plugin;
-		isOk = false;
 		loadKeys();
 	}
 
 	// load language yml
 	public synchronized void loadLanguage(final String lang) {
-		isOk = false;
+		this.isOk = false;
 		final String effectiveLang = (lang == null || lang.isEmpty() || lang.length() != 2) ? "en" : lang;
 		if(effectiveLang.length() != 2)
 			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Language should only be 2 letters! "+effectiveLang);
@@ -54,13 +52,13 @@ public class Language {
 			}
 		}
 		// try loading language file
-		loadLanguageFile(effectiveLang);
-		if(isOk) return;
+		this.loadLanguageFile(effectiveLang);
+		if(this.isOk) return;
 		if(!effectiveLang.equals("en")) {
 			// failed to load, so load default en.yml
 			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Defaulting to en.yml");
-			loadLanguageFile("en");
-			if(isOk) return;
+			this.loadLanguageFile("en");
+			if(this.isOk) return;
 		}
 		WebAuctionPlus.log.severe("Failed to load language! "+effectiveLang);
 	}
@@ -69,10 +67,10 @@ public class Language {
 	private void loadLanguageFile(final String lang) {
 		try {
 			// load from plugins folder
-			final File langFile = new File(plugin.getDataFolder().toString()+File.separator+"languages"+File.separator+lang+".yml");
-			this.langConfig = YamlConfiguration.loadConfiguration(langFile);
+			final File langFile = new File(this.plugin.getDataFolder().toString()+File.separator+"languages"+File.separator+lang+".yml");
+			final FileConfiguration langConfig = YamlConfiguration.loadConfiguration(langFile);
 			// load defaults from jar
-			final InputStream defaultLangStream = plugin.getClass().getClassLoader().getResourceAsStream("languages/"+lang+".yml");
+			final InputStream defaultLangStream = this.plugin.getClass().getClassLoader().getResourceAsStream("languages/"+lang+".yml");
 			if(defaultLangStream == null && !langFile.exists()) {
 				WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Language file not found: "+lang+".yml");
 				return;
@@ -91,8 +89,8 @@ public class Language {
 				}
 			}
 			// load language messages
-			for(final String key : langMap.keySet()) {
-				langMap.put(key, langConfig.getString(key));
+			for(final String key : this.langMap.keySet()) {
+				this.langMap.put(key, langConfig.getString(key));
 			}
 			WebAuctionPlus.log.info(WebAuctionPlus.logPrefix+"Loaded language file "+lang+".yml");
 
@@ -101,7 +99,7 @@ public class Language {
 			e.printStackTrace();
 			return;
 		}
-		isOk = true;
+		this.isOk = true;
 	}
 	private List<String> loadWordsFile(final String lang) {
 		// load from plugins folder
@@ -148,10 +146,10 @@ public class Language {
 	}
 
 	public synchronized String getString(final String key) {
-		if(!isOk) {
+		if(!this.isOk) {
 			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"No language file has been loaded!");
-		} else if(langMap.containsKey(key)) {
-			String value = langMap.get(key);
+		} else if(this.langMap.containsKey(key)) {
+			String value = this.langMap.get(key);
 			if(value!=null && !value.isEmpty())
 				return value;
 		}
@@ -160,35 +158,35 @@ public class Language {
 	}
 
 	private void loadKeys() {
-		langMap.put("no_permission",				"");
-		langMap.put("no_cheating",					"");
-		langMap.put("no_item_in_hand",				"");
-		langMap.put("item_stack_stored",			"");
-		langMap.put("got_mail",						"");
-		langMap.put("no_mail",						"");
-		langMap.put("inventory_full",				"");
-		langMap.put("not_enough_money_pocket",		"");
-		langMap.put("not_enough_money_account",		"");
-		langMap.put("reloading",					"");
-		langMap.put("finished_reloading",			"");
-		langMap.put("account_created",				"");
-		langMap.put("password_changed",				"");
-		langMap.put("account_not_found",			"");
-		langMap.put("created_shout_sign",			"");
-		langMap.put("created_recent_sign",			"");
-		langMap.put("created_deposit_sign",			"");
-		langMap.put("created_withdraw_sign",		"");
-		langMap.put("created_deposit_mail_sign",	"");
-		langMap.put("created_withdraw_mail_sign",	"");
-		langMap.put("sign_removed",					"");
-		langMap.put("invalid_sign",					"");
-		langMap.put("mailbox_title",				"");
-		langMap.put("mailbox_opened",				"");
-		langMap.put("mailbox_closed",				"");
-		langMap.put("please_wait",					"");
-		langMap.put("removed_enchantments",			"");
+		this.langMap.put("no_permission",               "");
+		this.langMap.put("no_cheating",                 "");
+		this.langMap.put("no_item_in_hand",             "");
+		this.langMap.put("item_stack_stored",           "");
+		this.langMap.put("got_mail",                    "");
+		this.langMap.put("no_mail",                     "");
+		this.langMap.put("inventory_full",              "");
+		this.langMap.put("not_enough_money_pocket",     "");
+		this.langMap.put("not_enough_money_account",    "");
+		this.langMap.put("reloading",                   "");
+		this.langMap.put("finished_reloading",          "");
+		this.langMap.put("account_created",             "");
+		this.langMap.put("password_changed",            "");
 		this.langMap.put("password_cmd_changed",        "");
 		this.langMap.put("password_generated",          "");
+		this.langMap.put("account_not_found",          "");
+		this.langMap.put("created_shout_sign",         "");
+		this.langMap.put("created_recent_sign",        "");
+		this.langMap.put("created_deposit_sign",       "");
+		this.langMap.put("created_withdraw_sign",      "");
+		this.langMap.put("created_deposit_mail_sign",  "");
+		this.langMap.put("created_withdraw_mail_sign", "");
+		this.langMap.put("sign_removed",               "");
+		this.langMap.put("invalid_sign",               "");
+		this.langMap.put("mailbox_title",              "");
+		this.langMap.put("mailbox_opened",             "");
+		this.langMap.put("mailbox_closed",             "");
+		this.langMap.put("please_wait",                "");
+		this.langMap.put("removed_enchantments",       "");
 	}
 
 
