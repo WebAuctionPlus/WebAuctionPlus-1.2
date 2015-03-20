2c9d87d86b55d56a9d527cecaa755aeb8783fe11
lorenzop <lorenzo@poixson.com>
2015-03-20 18:28:30 -0400

    added SALE_SERVER for server shop type to logSales class

--- www/WebInterface/inc/classes/logSales.class.php
+++ www/WebInterface/inc/classes/logSales.class.php
@@ -8,13 +8,14 @@ const LOG_CANCEL = 'cancel';
 
 const SALE_BUYNOW  = 'buynow';
 const SALE_AUCTION = 'auction';
+const SALE_SERVER  = 'server';
 
 
 public static function addLog($logType, $saleType, $sellerId, $buyerId, $Item, $price, $allowBids, $currentWinner, $alert=0){global $config;
   $query = "INSERT INTO `".$config['table prefix']."LogSales` ( ".
            "`logType`, `saleType`, `timestamp`, `itemType`, `itemId`, `itemDamage`, `itemTitle`, `enchantments`, `sellerId`, `buyerId`, `qty`, `price`, `alert` ) VALUES ( ".
            (($logType  == self::LOG_NEW     || $logType  == self::LOG_SALE || $logType == self::LOG_CANCEL) ? "'".mysql_san($logType )."'" : 'NULL' ).", ".
-           (($saleType == self::SALE_BUYNOW || $saleType == self::SALE_AUCTION                            ) ? "'".mysql_san($saleType)."'" : 'NULL' ).", ".
+           (($saleType == self::SALE_BUYNOW || $saleType == self::SALE_AUCTION || $saleType == self::SALE_SERVER) ? "'".mysql_san($saleType)."'" : 'NULL' ).", ".
            "NOW(), ".
            "'".mysql_san($Item->getItemType())."', ".
            ((int) $Item->getItemId()).", ".
