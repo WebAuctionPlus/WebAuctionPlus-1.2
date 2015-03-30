54712afffdfaa060aaaefc824c63f7b42885c467
lorenzop <lorenzo@poixson.com>
2015-03-30 15:47:55 -0400

    fixed bugs in logSales class when $Item is null

--- www/WebInterface/inc/classes/logSales.class.php
+++ www/WebInterface/inc/classes/logSales.class.php
@@ -17,14 +17,14 @@ public static function addLog($logType, $saleType, $sellerId, $buyerId, $Item, $
            (($logType  == self::LOG_NEW     || $logType  == self::LOG_SALE     || $logType == self::LOG_CANCEL)   ? "'".mysql_san($logType )."'" : 'NULL' ).", ".
            (($saleType == self::SALE_BUYNOW || $saleType == self::SALE_AUCTION || $saleType == self::SALE_SERVER) ? "'".mysql_san($saleType)."'" : 'NULL' ).", ".
            "NOW(), ".
-           "'".mysql_san($Item->getItemType())."', ".
-           ((int) $Item->getItemId()).", ".
-           ((int) $Item->getItemDamage()).", ".
-           "'".mysql_san($Item->getItemTitle())."', ".
-           "'".mysql_san($Item->getEnchantmentsCompressed())."', ".
-           ($sellerId == NULL ? 'NULL' : "'".mysql_san($sellerId)."'").", ".
-           ($buyerId  == NULL ? 'NULL' : "'".mysql_san($buyerId)."'").", ".
-           ((int) $Item->getItemQty()).", ".
+           ( $Item     == NULL ? 'NULL' : "'".mysql_san($Item->getItemType())."'"               ).", ".
+           ( $Item     == NULL ? '0'    : ((int) $Item->getItemId())                            ).", ".
+           ( $Item     == NULL ? '0'    : ((int) $Item->getItemDamage())                        ).", ".
+           ( $Item     == NULL ? 'NULL' : "'".mysql_san($Item->getItemTitle())."'"              ).", ".
+           ( $Item     == NULL ? 'NULL' : "'".mysql_san($Item->getEnchantmentsCompressed())."'" ).", ".
+           ( $sellerId == NULL ? 'NULL' : "'".mysql_san($sellerId)."'"                          ).", ".
+           ( $buyerId  == NULL ? 'NULL' : "'".mysql_san($buyerId)."'"                           ).", ".
+           ( $Item     == NULL ? '0'    : ((int) $Item->getItemQty())                           ).", ".
            ((double) $price).", ".
            ((int) $alert)." )";
   $result = RunQuery($query, __file__, __line__);
