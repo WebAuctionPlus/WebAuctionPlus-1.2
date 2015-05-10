2086e689eae015bbf46eb54bb08c995879f45875
lorenzop <lorenzo@poixson.com>
2015-05-10 01:38:15 -0400

    added getItemBlacklist() function to waSettings added "Item Blacklist" to Settings table

--- java/src/me/lorenzop/webauctionplus/waSettings.java
+++ java/src/me/lorenzop/webauctionplus/waSettings.java
@@ -4,11 +4,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class waSettings {
 
 	protected HashMap<String, String> settingsMap = new HashMap<String, String>();
+	protected ItemStack[] itemBlacklist = null;
 
 	private final WebAuctionPlus plugin;
 	private boolean isOk;
@@ -64,6 +70,7 @@ public class waSettings {
 //		addDefault("Max Selling Per Player",20);
 //		addDefault("Storage base per stack",1.0);
 //		addDefault("Storage add per item",  0.1);
+		addDefault("Item Blacklist",     "");
 	}
 	private void addDefault(String name, String value) {
 		if(!this.settingsMap.containsKey(name)) {
@@ -166,6 +173,71 @@ public class waSettings {
 	public void setBoolean(String name, boolean value) {
 		this.setString(name, Boolean.toString(value));
 	}
+
+
+
+	public ItemStack[] getItemBlacklist() {
+		if(this.itemBlacklist != null)
+			return this.itemBlacklist;
+		final String str = this.getString("Item Blacklist");
+		if(str == null || str.isEmpty()) {
+			this.itemBlacklist = new ItemStack[0];
+			return this.itemBlacklist;
+		}
+		final List<ItemStack> list = new ArrayList<ItemStack>();
+		final String parts[] = str.split(",");
+		for(String part : parts) {
+			part = part.trim();
+			if(part.isEmpty()) continue;
+			final ItemStack stack = getItemBlacklist_Type(part);
+			if(stack == null) continue;
+			list.add(stack);
+		}
+		this.itemBlacklist = list.toArray(new ItemStack[0]);
+		return this.itemBlacklist;
+	}
+	@SuppressWarnings("deprecation")
+	private static ItemStack getItemBlacklist_Type(final String str) {
+		if(str == null || str.isEmpty())
+			return null;
+		final String typeStr;
+		Material mat;
+		final short damage;
+		if(str.contains(":")) {
+			final String[] p = str.split(":");
+			typeStr = p[0];
+			try {
+				damage = Short.parseShort(p[1]);
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid backlisted item: "+str);
+				e.printStackTrace();
+				return null;
+			}
+		} else {
+			typeStr = str;
+			damage = -1;
+		}
+		try {
+			final int id = Integer.parseInt(typeStr);
+			mat = Material.getMaterial(id);
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid backlisted item: "+str);
+			e.printStackTrace();
+			return null;
+			// don't use this, can't be supported on php side
+//			try {
+//				mat = Material.valueOf(typeStr);
+//			} catch (IllegalArgumentException e2) {
+//				System.out.println("Invalid backlisted item: "+str);
+//				e2.printStackTrace();
+//				return null;
+//			}
+		}
+		if(damage == -1)
+			return new ItemStack(mat, 1);
+		return new ItemStack(mat, 1, damage);
+	}
+
 
 
 }
